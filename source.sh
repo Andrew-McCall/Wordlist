85c5d62f2ce7dd230b041bdd3e85a6420bee6d3c
Andrew-McCall <andrewdavidmccall@hotmail.com>
2024-12-29 15:31:35 +0000

    Sources

---
+++ source.sh
@@ -0,0 +1,6 @@
+#Downloaded from Project Gutenberg - Moby Thesaurus II
+#http://onlinebooks.library.upenn.edu/webbin/gutbook/lookup?num=3202
+
+wget https://www.gutenberg.org/dirs/3/2/0/3202/files/mthesaur.txt
+
+
